ef7f332482113d0ecb579194bc9bf652b59bc5b0
fiza-khalid567 <127833941+fiza-khalid567@users.noreply.github.com>
2023-03-17 22:33:52 +0530

    Add files via upload
    data types in java script

---
+++ 7.data types.js
@@ -0,0 +1,33 @@
+var x="This is data type";
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
+document.write("<br>");
+
+y=34;
+document.write(y);
+document.write("<br>");
+document.write(typeof y);
+document.write("<br>");
+
+z=true;
+document.write(z);
+document.write("<br>");
+document.write(typeof z);
+document.write("<br>");
+
+a=["fiza,radhika,neha"];
+document.write(a);
+document.write("<br>");
+document.write(typeof a);
+document.write("<br>");
+
+b={first:"fiza",last:"khan"};
+document.write(b);
+document.write("<br");
+document.write(typeof b);
+document.write("<br>");
+
+
+
+
