0ac0d8baecf559822f9b7ecf8710c5feda18944d
fiza-khalid567 <127833941+fiza-khalid567@users.noreply.github.com>
2023-03-17 22:32:28 +0530

    Add files via upload
    let or constant variables

---
+++ 6.let or const variables.js
@@ -0,0 +1,6 @@
+let first="<h3><b><i>This is let variable</b></i></h3>";
+let first="This is also"//let variable declared only one time
+document.write(first);
+
+const second="this is const variable";
+document.write(second);
