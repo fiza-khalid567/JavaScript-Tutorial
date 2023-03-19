ff2bcf75416bb269643bdf1612ce204f3dc64e8c
fiza-khalid567 <127833941+fiza-khalid567@users.noreply.github.com>
2023-03-19 21:01:48 +0530

    Add files via upload
    assignment operator in java script

---
+++ 9.assignment operator.js
@@ -0,0 +1,49 @@
+//= assignment operator
+var a=10;
+var b=20;
+var c=a+b;
+document.write("addition is-");
+document.write(c);
+document.write("<br>");
+
+//+= assignment operator
+var a=10;
+var b=20;
+a+=b;
+document.write(a);
+document.write("<br>");
+
+//-= assignment operator
+var a=30;
+var b=20;
+a-=b;
+document.write(a);
+document.write("<br>");
+
+//*= assignment operator
+var a=10;
+var b=20;
+a*=b;
+document.write(a);
+document.write("<br>");
+
+// /= assignment operator
+var a=10;
+var b=20;
+a/=b;
+document.write(a);
+document.write("<br>");
+
+// %=assignment operators
+var a=10;
+var b=2;
+a%=b;
+document.write(a);
+document.write("<br>");
+
+// **assignment opwrator
+var a=10;
+var b=2;
+a**=b;
+document.write(a);
+document.write("<br>");
